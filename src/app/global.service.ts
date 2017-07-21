9841cd81460506a3a28e5dc99f64ffd08cd4443d
Wilson Santos <wilsonsantosnet@gmail.com>
2017-07-21 12:00:19 -0300

    ++

--- src/app/global.service.ts
+++ src/app/global.service.ts
@@ -1,4 +1,4 @@
-﻿import { Injectable, EventEmitter } from '@angular/core'
+import { Injectable, EventEmitter } from '@angular/core'
 
 export class GlobalService {
 
@@ -43,12 +43,15 @@ export class EndPoints {
     public readonly AUTHAPI: string;
     public readonly AUTH: string;
     public readonly APP: string;
+    public readonly DOWNLOAD: string;
 
     constructor() {
         this.DEFAULT = 'http://localhost:8122/api';
         this.AUTHAPI = 'http://localhost:4000/api';
         this.AUTH = 'http://localhost:4000/';
         this.APP = 'http://localhost:4200';
+        this.DOWNLOAD = this.DEFAULT + + "document/download/";
+
     }
 };
 
@@ -61,7 +64,7 @@ export class AuthSettings {
 
     constructor() {
         this.TYPE_LOGIN = "SSO";
-        this.CLIENT_ID = 'client-id';
+        this.CLIENT_ID = 'Target-spa-v2';
         this.SCOPE = 'ssosa';
     
     }
