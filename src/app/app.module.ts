a71797756d630c7f55728e32a3a81717d930ee11
Wilson Santos <wilsonsantosnet@gmail.com>
2017-08-22 18:22:06 -0300

    ++

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { SimpleNotificationsModule } from 'angular2-notifications';
-
-import { routingDefault } from './app.routing';
-import { routingCustom } from './app.custom.routing';
+import { ConfirmModalComponent } from 'app/common/components/confirm-modal.component';
+import { RoutingDefault } from './app.routing';
+import { RoutingCustom } from './app.custom.routing';
 import { LoginComponent } from './login/login.component';
 import { AuthService } from 'app/common/services/auth.service'
 import { ApiService } from 'app/common/services/api.service';
