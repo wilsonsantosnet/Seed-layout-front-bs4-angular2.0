7cf97ff4a8e2457aff41edbe9ba19adf7fc7e48d
Wilson Santos <wilsonsantosnet@gmail.com>
2017-08-24 11:42:30 -0300

    ++

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,6 +1,14 @@
 /* SystemJS module definition */
+
+declare module "*.json" {
+    const value: any;
+    export default value;
+}
+
 declare var module: NodeModule;
+
 declare var tinymce: any;
+
 interface NodeModule {
   id: string;
 }
