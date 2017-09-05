2293030e24cf65a73dd12139afc3e940ee4499e6
Wilson Santos <wilsonsantosnet@gmail.com>
2017-09-05 18:27:48 -0300

    new configurations External feature

---
+++ src/app/startup.service.ts
@@ -0,0 +1,30 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { GlobalService } from 'app/global.service';
+
+@Injectable()
+export class StartupService {
+
+
+    constructor(private http: Http) { }
+
+    load(): Promise<any> {
+
+        return new Promise((resolve: any) => {
+
+            let jsonFileURL: string = "assets/appsettings.json";
+            this.http.get(jsonFileURL)
+                .map((res: Response) => res.json())
+                .toPromise()
+                .then((data: any) => {
+                    return resolve(GlobalService.setEndPoints(data.ConfigSettings));
+                })
+                .catch((err: any) => {
+                    Promise.resolve()
+                });
+        });
+    }
+
+
+
+}
