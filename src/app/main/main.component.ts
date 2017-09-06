5c796d1771932c24377ab66ea75e76a5fb4078c4
Wilson Santos <wilsonsantosnet@gmail.com>
2017-09-06 12:00:44 -0300

    ++

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,34 +1,52 @@
-﻿import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'app/common/services/auth.service'
+import { GlobalServiceCulture } from '../global.service.culture'
+import { GlobalService } from '../global.service'
+import { MainService } from './main.service';
 
 @Component({
     selector: 'app-main',
     templateUrl: './main.component.html',
-    styleUrls: ['./main.component.css']
+    styleUrls: ['./main.component.css'],
+    providers: [GlobalServiceCulture, MainService]
 })
 export class MainComponent implements OnInit {
 
     vm: any;
     menuIsOpen: boolean;
-    constructor(private authService: AuthService) {
+    constructor(private authService: AuthService, private globalServiceCulture: GlobalServiceCulture, private mainService: MainService) {
+
         this.vm = {};
         this.menuIsOpen = true;
+        this.vm.generalInfos = this.mainService.getGeneralInfos();
+
+        GlobalService.changeCulture.subscribe((culture) => {
+            this.mainService.updateCulture(this.vm, culture);
+        });
+    }
+
+    onUpdateCulture(event, culture: string) {
+
+        event.preventDefault();
+        this.mainService.updateCulture(this.vm, culture);
+        GlobalService.changeCulture.emit(culture);
+
     }
 
     ngOnInit() {
-        this.authService.menu((result) => {
 
-            if (result.data != null) {
-                if (result.data.name != null) {
-                    this.vm.userName = result.data.name
+        this.authService.getCurrentUser((result) => {
+            if (result.isAuth) {
+                if (result.claims.name != null) {
+                    this.vm.userName = result.claims.name
                 }
 
-                if (result.data.role != null) {
-                    this.vm.userRole = result.data.typerole
+                if (result.claims.role != null) {
+                    this.vm.userRole = result.claims.role
                 }
 
-                if (result.data.tools != null) {
-                    this.vm.menu = JSON.parse(result.data.tools)
+                if (result.claims.tools != null) {
+                    this.vm.menu = JSON.parse(result.claims.tools)
                 }
             }
 
@@ -40,7 +58,7 @@ export class MainComponent implements OnInit {
         this.menuIsOpen = !this.menuIsOpen
     }
 
-  onLogout(e) {
+    onLogout(e) {
         e.preventDefault();
         this.authService.logout();
     }
